c294aca7f1af8a023995f7770c0644fc070173c4
William Valadez <billy.valadez@icloud.com>
2023-05-18 21:00:32 -0400

    fixed minor issues in timeWord.js

--- timeWord.js
+++ timeWord.js
@@ -1,20 +1,3 @@
-/**
- *
- * Input	Expected Output
- * 00:00	midnight
- * 00:12	twelve twelve am
- * 01:00	one o’clock am
- * 06:01	six oh one am
- * 06:10	six ten am
- * 06:18	six eighteen am
- * 06:30	six thirty am
- * 10:34	ten thirty four am
- * 12:00	noon
- * 12:09	twelve oh nine pm
- * 23:23	eleven twenty three pm
- *
- */
-
 const hoursObj = {
   00: "twelve",
   01: "one",
@@ -99,10 +82,11 @@ const timeToWords = (time) => {
     time === "12:00" ? (hourText = "noon") : (hourText = "midnight");
     return hourText;
   } else {
+    if (tens.concat(ones) === "00") minsText = "o'clock";
     hourText = hoursObj[hours];
     hours >= 12 ? (timeOfDay = "pm") : (timeOfDay = "am");
     return `${hourText} ${minsText} ${timeOfDay}`;
   }
 };
 
-console.log(timeToWords("12:00"));
+console.log(timeToWords("01:01"));
